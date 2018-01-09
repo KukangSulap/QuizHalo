e333925d55928217ed766a5c00658374a370497d
KukangSulap <32661586+KukangSulap@users.noreply.github.com>
2018-01-09 11:07:02 +0700

    f
    s

--- app/src/main/java/com/example/barney/quizhalo/MainActivity.java
+++ app/src/main/java/com/example/barney/quizhalo/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     String d = "txt4";
     String e = "txt5";
     String f = "txt6";
-    String h = "txt7";
-    String i = "txt8";
+    String g = "txt7";
+    String h = "txt8";
 
     TextView txt1,txt2,txt3,txt4,txt5,txt6,txt7,txt8;
 
@@ -22,5 +22,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt1 = (TextView) findViewById(R.id.txt1);
+        txt2 = (TextView) findViewById(R.id.txt2);
+        txt3 = (TextView) findViewById(R.id.txt3);
+        txt4 = (TextView) findViewById(R.id.txt4);
+        txt5 = (TextView) findViewById(R.id.txt5);
+        txt6 = (TextView) findViewById(R.id.txt6);
+        txt7 = (TextView) findViewById(R.id.txt7);
+        txt8 = (TextView) findViewById(R.id.txt8);
+
+
+        txt1.setText(a);
+        txt2.setText(b);
+        txt3.setText(c);
+        txt4.setText(d);
+        txt5.setText(e);
+        txt6.setText(f);
+        txt7.setText(g);
+        txt8.setText(h);
     }
 }
